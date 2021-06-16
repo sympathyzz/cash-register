96d5eab339d63f08487708917290f1769dcd3b3e
GAOGA4 <gaovin.z.l.gao@oocl.com>
2021-06-16 19:47:18 +0800

    completed test should_process_execute_printing_by_spy

--- src/test/java/com/tw/academy/CashRegisterTest.java
+++ src/test/java/com/tw/academy/CashRegisterTest.java
@@ -23,4 +23,29 @@ class CashRegisterTest {
 		verify(printer).print(printResult);
 	}
 
+	@Test
+	void should_process_execute_printing_by_spy() {
+		//given
+		SpyPrinter spyPrinter=new SpyPrinter();
+		CashRegister cashRegister = new CashRegister(spyPrinter);
+		Purchase purchase = new Purchase();
+		//when
+		cashRegister.process(purchase);
+		//then
+		//verify that cashRegister.process will trigger print
+		assertTrue(spyPrinter.hasExecutePrint);
+	}
+
+
+
+	private class SpyPrinter extends Printer {
+		boolean hasExecutePrint=false;
+
+		@Override
+		public void print(String content) {
+			hasExecutePrint=true;
+		}
+	}
+
+
 }
